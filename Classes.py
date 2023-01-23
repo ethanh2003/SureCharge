476333ca8034bc22b1a1143bcf975ea47838fc13
Ethan Herring <eherrin6@uncc.edu>
2023-01-23 10:20:37 -0500

    Added classes, implimented saving and reading data.

---
+++ Classes.py
@@ -0,0 +1,40 @@
+
+class User:
+    def __init__(self, user_id, name, pin, accessLevel, payrate, hoursWorked, clock_in):
+        self.clock_in = clock_in
+        self.user_id = user_id
+        self.name = name
+        self.pin = pin
+        self.accessLevel = accessLevel
+        self.payrate = payrate
+        self.hoursWorked = hoursWorked
+
+
+class Product:
+    def __init__(self, product_id, name, price, costToMake, tax):
+        self.product_id = product_id
+        self.name = name
+        self.price = price
+        self.costToMake = costToMake
+        self.tax = tax
+
+
+class Sale:
+    def __init__(self, checkNum, date, time, products, user, paymentType, paymentAmount, tax, discount, refunded):
+        self.refunded = refunded
+        self.discount = discount
+        self.tax = tax
+        self.paymentAmount = paymentAmount
+        self.paymentType = paymentType
+        self.user = user
+        self.products = products
+        self.time = time
+        self.date = date
+        self.checkNum = checkNum
+
+
+class GiftCard:
+    def __init__(self, cardNum, startAmount, currentBalance):
+        self.currentBalance = currentBalance
+        self.startAmount = startAmount
+        self.cardNum = cardNum
--- app.py
+++ app.py
@@ -1,39 +1,116 @@
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 import csv
+from Classes import *
+from datetime import datetime
 
 
-def youClicked():
-    label.setText("you clicked me")
-
-
-user_list = {'id': 1, 'name': 'Admin', 'pin': 9999, 'accessLevel': 0, 'payrate': 20, 'hoursWorked': 0}
-
-
-class User:
-    def __init__(self, id, name, pin, accessLevel, payrate, hoursWorked):
-        self.id = id
-        self.name = name
-        self.pin = pin
-        self.accessLevel = accessLevel
-        self.payrate = payrate
-        self.hoursWorked = hoursWorked
-
-
-user_list = [User(1, 'Admin', 9999, 0, 20, 0)]
-
+user_list = []
+product_list = []
+sale_records = []
+giftCards = []
 
 app = QApplication([])  # Start an application.
 
+def clockOut(user):
+    clock_in_time = datetime.strptime(user.clock_in, '%H:%M')
+    hoursWorked = datetime.now() - clock_in_time
+    hoursWorked = round(hoursWorked.total_seconds() / 3600)
+    user.hoursWorked += hoursWorked
+
+def exitApp():
+    app.exit
+    saveData()
 
 def saveData():
-    with open('user_file.csv', mode='w') as user_file:
-        fieldnames = ['id', 'name', 'pin', 'accessLevel', 'payrate', 'hoursWorked']
-        user_writer = csv.DictWriter(user_file, fieldnames=fieldnames)
-        user_writer.writerow(user_list)
+    with open('csv_files/user_file.csv', mode='w', newline='') as user_file:
+        fieldnames = ['user_id', 'name', 'pin', 'accessLevel', 'payrate', 'hoursWorked', 'clock-in']
+
+        user_writer = csv.writer(user_file)
+        user_writer.writerow(fieldnames)
+        for user in user_list:
+            user_writer.writerow(
+                (user.user_id, user.name, user.pin, user.accessLevel, user.payrate, user.hoursWorked, user.clock_in))
+    with open('csv_files/product_file.csv', mode='w', newline='') as product_file:
+        fieldnames = ['product_id', 'name', 'price', 'costToMake', 'tax']
+
+        product_writer = csv.writer(product_file)
+        product_writer.writerow(fieldnames)
+        for product in product_list:
+            product_writer.writerow((product.product_id, product.name, product.price, product.costToMake, product.tax))
+    with open('csv_files/giftcards_file.csv', mode='w', newline='') as giftcards_file:
+        fieldnames = ['cardNum', 'startAmount', 'currentBalance']
+
+        giftcard_writer = csv.writer(giftcards_file)
+        giftcard_writer.writerow(fieldnames)
+        for cards in giftCards:
+            giftcard_writer.writerow((cards.cardNum, cards.startAmount, cards.currentBalance))
 
 
-saveData()
+with open('csv_files/sales_file.csv', mode='w', newline='') as sales_file:
+    fieldnames = ['checkNum', 'date', 'time', 'products', 'user', 'paymentType', 'paymentAmount', 'tax', 'discount',
+                  'refunded']
+
+    sales_writer = csv.writer(sales_file)
+    sales_writer.writerow(fieldnames)
+    for sale in sale_records:
+        sales_writer.writerow((sale.checkNum, sale.date, sale.time, sale.products, sale.user, sale.paymentType,
+                               sale.paymentAmount, sale.tax, sale.discount, sale.refunded))
+
+
+def readData():
+    with open('csv_files/user_file.csv', 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+        fields = []
+        rows = []
+        # extracting field names through first row
+        fields = next(csvreader)
+
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+        for row in rows:
+            user_list.append(User(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+    with open('csv_files/product_file.csv', 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+        fields = []
+        rows = []
+        # extracting field names through first row
+        fields = next(csvreader)
+
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+        for row in rows:
+            product_list.append(Product(row[0], row[1], row[2], row[3], row[4]))
+    with open('csv_files/sales_file.csv', 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+        fields = []
+        rows = []
+        # extracting field names through first row
+        fields = next(csvreader)
+
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+        for row in rows:
+            sale_records.append(Sale(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]))
+    with open('csv_files/giftcards_file.csv', 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+        fields = []
+        rows = []
+        # extracting field names through first row
+        fields = next(csvreader)
+
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+        for row in rows:
+            giftCards.append(GiftCard(row[0], row[1], row[2]))
 
 
 def setLightMode():
@@ -59,16 +136,16 @@ def setDarkMode():
     app.setPalette(dark_palette)
 
 
+readData()
+setDarkMode()
 window = QWidget()  # Create a window.
 layout = QVBoxLayout()  # Create a layout.
 
-button = QPushButton("I'm just a Button man")  # Define a button
+button = QPushButton("Exit")  # Define a button
 
-label = QLabel('¯\_(ツ)_/¯')
-button.clicked.connect(youClicked)
+button.clicked.connect(exit)
 layout.addWidget(QLabel('Hello World!'))  # Add a label
 layout.addWidget(button)  # Add the button man
-layout.addWidget(label)
 
 window.setLayout(layout)  # Pass the layout to the window
 window.show()  # Show window
